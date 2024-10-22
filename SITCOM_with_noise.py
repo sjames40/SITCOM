c94638fa9290536030e2abc6914cd590477a34eb
sjames40 <54916856+sjames40@users.noreply.github.com>
2024-10-22 12:55:39 -0400

    Update SITCOM_with_noise.py

--- SITCOM_with_noise.py
+++ SITCOM_with_noise.py
@@ -26,7 +26,7 @@ def load_yaml(file_path: str) -> dict:
 
 model_config='/home/shijun.liang/diffusion_github_code/Measurment_Consistent_Diffusion_Trajectory/configs/model_config.yaml'
 diffusion_config='/home/shijun.liang/diffusion_github_code/Measurment_Consistent_Diffusion_Trajectory/configs/diffusion_config.yaml'
-task_config= '/home/shijun.liang/diffusion_github_code/Measurment_Consistent_Diffusion_Trajectory/configs/super_resolution_config.yaml'
+task_config= '/home/shijun.liang/diffusion_github_code/Measurment_Consistent_Diffusion_Trajectory/configs/motion_deblur_config.yaml'
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Load configurations
